a183cf9d0eec7e6bd32f25017f80e54ab3fe1165
Franziska Pätzold <Franziska.Pae@gmail.com>
2025-02-07 20:56:37 +0100

    update server linked to frontend domain

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,  
+    methods: ["POST"],
+    allowedHeaders: ["Content-Type"],
+}));
 
 
 const openai = new OpenAI({
